74186c1c335b405b814ff498f19c94205eae92f5
KatherineLexie <yuhan990105@163.com>
2020-11-15 13:43:31 -0800

    changes

--- train_models/train_autoencoder.py
+++ train_models/train_autoencoder.py
@@ -30,16 +30,6 @@ from tensorflow.keras.callbacks import ModelCheckpoint
 target_size = (64, 64)
 import argparse
 
-
-def Input_Image_Flow(batchsize,targetsize):
-    from keras.preprocessing.image import ImageDataGenerator
-    # create generator
-    datagen = ImageDataGenerator()
-    # prepare an iterators for each dataset
-    input_img = datagen.flow_from_directory(train_dir, class_mode='binary',batch_size=batchsize,target_size=targetsize)
-    batchX, batchy = train_it.next()
-    print('Batch shape=%s, min=%.3f, max=%.3f' % (batchX.shape, batchX.min(), batchX.max()))
-
 def random_crop(image):
     cropped_image = tf.image.random_crop(image, size=[target_size[0], target_size[1], 3])
     cropped_image = tf.image.resize(cropped_image, (image.shape[0], image.shape[1]))
@@ -110,14 +100,6 @@ if __name__ == '__main__':
     train_dir = base_dir + 'data/train'
     validation_dir = base_dir + 'data/val'
     test_dir = base_dir + 'data/test'
-
-    parse = argparse.ArgumentParser()
-    parse.add_argument("-d","--directory",dest="target",help="base directory",default='/deac/classes/csc391/langrc18/art-style-conversion/')
-    parse.add_argument("-bs","--batch_size",dest="batch_size",help="batch size for input images",default=32)
-    parse.add_argument("-ts","--target_size",dest="target_size",help="target size for input images",default=(64,64))
-    args = parse.parse_args()
-    batch_size = args.batch_size
-    target_size = args.target_size
 
     print('Data Generator')
     train_datagen = ImageDataGenerator(
